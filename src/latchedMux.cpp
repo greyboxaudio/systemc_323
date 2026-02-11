4adfd77431570b7c9414c801613876ff36e3d44f
Robert Vettel <ra.vettel@outlook.com>
2026-02-11 19:10:09 +0100

    fixed latched mux

--- src/latchedMux.cpp
+++ src/latchedMux.cpp
@@ -1,14 +1,21 @@
 #include "latchedMux.h"
+void latchedMux::latchedMux_read(void)
+{
+    sc_uint<8> inputA = inp0.read();
+    sc_uint<8> inputB = (inp1.read() & 0x7f) << 1;
+    bool byteSelect = sel.read();
 
-    void latchedMux::latchedMux_prc(void)
+    if (byteSelect)
     {
-        sc_uint<8> inputA = inp0.read();
-        sc_uint<8> inputB = (inp1.read() & 0x7f) << 1;
-        bool byteSelect = sel.read();
-        if (byteSelect)
-        {
-            outp0.write(inputB);
-        }else{
-            outp0.write(inputA);
-        }
+        data = inputB;
     }
+    else
+    {
+        data = inputA;
+    }
+}
+void latchedMux::latchedMux_latch(void)
+{
+
+    outp0.write(data);
+}
--- src/latchedMux.h
+++ src/latchedMux.h
@@ -4,12 +4,15 @@ SC_MODULE(latchedMux)
     sc_in<bool> clk, sel;
     sc_in<sc_uint<8>> inp0, inp1;
     sc_out<sc_uint<8>> outp0;
-
+    sc_uint<8> data;
     void latchedMux_prc();
+    void latchedMux_read();
 
     SC_CTOR(latchedMux)
     {
-        SC_METHOD(latchedMux_prc);
+        SC_METHOD(latchedMux_read);
+        sensitive <<sel << inp0 << inp1;
+        SC_METHOD(latchedMux_latch);
         sensitive << clk.neg();
     }
 };
